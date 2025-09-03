538182e5bfc9627d635bc94a0a61595f31af8525
vasavifullstack <vasavi.fullstack@gmail.com>
2025-09-03 19:35:39 +0530

    data

--- script.js
+++
@@ -1,4 +0,0 @@
-console.log("hii");
-console.log("hi vasavireddy");
-var name="vasavireddy";
-console.log("name");
---
+++ variables.js
@@ -0,0 +1,6 @@
+console.log("hii");
+console.log("vasavireddy");
+var name="vasavireddy";
+console.log("name");
+let name1="rajitha";
+console.log("name1");
